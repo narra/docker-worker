3d82bedc0cfc6e361da0355be88b2af83f887ad8
Michal Mocnak <michal@marigan.net>
2020-10-31 10:57:57 +0100

    ffmpeg install fix to use latest version

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -5,5 +5,5 @@ set -x
 cd /opt
 wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
 tar xvf ffmpeg-release-amd64-static.tar.xz
-cp ffmpeg-4.2.2-amd64-static/ffmpeg /usr/bin/ffmpeg
-cp ffmpeg-4.2.2-amd64-static/ffprobe /usr/bin/ffprobe
+cp ffmpeg-*-amd64-static/ffmpeg /usr/bin/ffmpeg
+cp ffmpeg-*-amd64-static/ffprobe /usr/bin/ffprobe
